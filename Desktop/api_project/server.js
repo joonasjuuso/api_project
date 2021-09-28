e7fbe2896b763f947f80c0b30e50ef01cc104ae0
DESKTOP-RA8474D\joona <t9jujo00@students.oamk.fi>
2021-09-28 14:50:19 +0300

    small changes

--- Desktop/api_project/server.js
+++ Desktop/api_project/server.js
@@ -4,22 +4,49 @@ const port = 8000;
 
 
 const items = [ {
-    title: "",
-    description: "",
-    category: "",
-    location: "",
-    images: "",
-    price: "",
-    date: "",
-    delivery: "",
-    information: ""
- }]
+    title: "yes",
+    description: "yes",
+    category: "23",
+    location: "4444",
+    images: ["2","2"],
+    price: "555",
+    date: "124",
+    delivery: "23",
+    information: "2323"
+ },
+    {
+        title: "aaaa",
+        description: "sdsdadsades",
+        category: "23",
+        location: "44353544",
+        images: ["2","2"],
+        price: "55567555",
+        date: "124",
+        delivery: "23",
+        information: "2323" 
+    }]
 app.get('/', (req, res) => {
   res.send("Hello");
 })
 
 app.get('/items', (req, res) => {
+    res.json(dogs)
+})
 
+app.post('/items', (req, res) => {
+    res.send(dogs)
+})
+
+app.get('/items/:category', (req, res) => {
+    const category = items.find(item => item.category === req.params.category)
+})
+
+app.get('/items/:location', (req, res) => {
+    const location = items.find(item => item.location === req.params.location)
+})
+
+app.get('/items/:date', (req, res) => {
+    const date = items.find(item => item.date === req.params.date)
 })
 
 /* Header reader */
